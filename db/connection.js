7a0809fd6adcd92a2e25fbee50559a5d239abc76
Cristian Reyes <cristianreyes6777@gmail.com>
2023-12-08 20:30:06 -0500

    env file for security

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql2');
+const dotenv = require('dotenv'); 
+
+dotenv.config();
+
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', 
-    password: 'password', 
-    database: 'employee_tracker'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 connection.connect(err => {
--- index.js
+++ index.js
@@ -1,58 +1,77 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 const db = require('./db/connection');
 const Department = require('./lib/Department');
 const Role = require('./lib/Role');
 const Employee = require('./lib/Employee');
+const mysql = require("mysql2");
 
-function mainMenu() {
-    inquirer.prompt({
-        type: "list",
-        name: "action",
-        message: "What would you like to do?",
-        choices: [
-            "View all departments",
-            "View all roles",
-            "View all employees",
-            "Add a department",
-            "Add a role",
-            "Add an employee",
-            "Update an employee role",
-            "Exit"
-        ]
-    }).then((answer) => {
-        switch (answer.action) {
-            case "View all departments":
-                viewAllDepartments();
-                break;
-            case "View all roles":
-                viewAllRoles();
-                break;
-            case "View all employees":
-                viewAllEmployees();
-                break;
-            case "Add a department":
-                addDepartment();
-                break;
-            case "Add a role":
-                addRole();
-                break;
-            case "Add an employee":
-                addEmployee();
-                break;
-            case "Update an employee role":
-                updateEmployeeRole();
-                break;
-            case "Exit":
-                db.end();
-                console.log("Goodbye!");
-                break;
-            default:
-                console.log(`Invalid action: ${answer.action}`);
-                break;
-        }
-    });
+function start() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Add a Manager",
+                "Update an employee role",
+                "View Employees by Manager",
+                "View Employees by Department",
+                "Delete Departments | Roles | Employees",
+                "View the total utilized budget of a department",
+                "Exit",
+            ],
+        })
+        .then((answer) => {
+            switch (answer.action) {
+                case "View all departments":
+                    viewAllDepartments();
+                    break;
+                case "View all roles":
+                    viewAllRoles();
+                    break;
+                case "View all employees":
+                    viewAllEmployees();
+                    break;
+                case "Add a department":
+                    addDepartment();
+                    break;
+                case "Add a role":
+                    addRole();
+                    break;
+                case "Add an employee":
+                    addEmployee();
+                    break;
+                case "Add a Manager":
+                    addManager();
+                    break;
+                case "Update an employee role":
+                    updateEmployeeRole();
+                    break;
+                case "View Employees by Manager":
+                    viewEmployeesByManager();
+                    break;
+                case "View Employees by Department":
+                    viewEmployeesByDepartment();
+                    break;
+                case "Delete Departments | Roles | Employees":
+                    deleteDepartmentsRolesEmployees();
+                    break;
+                case "View the total utilized budget of a department":
+                    viewTotalUtilizedBudgetOfDepartment();
+                    break;
+                case "Exit":
+                    connection.end();
+                    console.log("Goodbye!");
+                    break;
+            }
+        });
 }
-
 
 function viewAllDepartments() {
     const department = new Department();
@@ -184,4 +203,4 @@ function addEmployee() {
 
 
 
-mainMenu();
+start();
